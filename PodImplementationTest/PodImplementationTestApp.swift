ee4efed80d2decd802fcd05700bea83beca93273
syed.wajahat <syed.wajahat@tpsonline.com>
2024-02-10 12:12:19 +0500

    Initial Commit

---
+++ PodImplementationTest/PodImplementationTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PodImplementationTestApp.swift
+//  PodImplementationTest
+//
+//  Created by Syed Wajahat Ali on 10/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct PodImplementationTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
