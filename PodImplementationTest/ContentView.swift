3d9f5d5d5c245c259664990ae5516216752c1b77
syed.wajahat <syed.wajahat@tpsonline.com>
2024-02-10 19:13:41 +0500

    Removed the commented code

--- PodImplementationTest/ContentView.swift
+++ PodImplementationTest/ContentView.swift
@@ -43,29 +43,6 @@ struct ContentView: View {
     }
 }
 
-//public struct _BottomSheetView<Content: View>: View {
-//    let maxHeight: CGFloat
-//    let minHeight: CGFloat
-//    let offset: CGFloat
-//    let content: Content
-//    
-//    public var body: some View {
-//        content
-//            .frame(maxWidth: .infinity, maxHeight: maxHeight, alignment: .center)
-//            .background(Color.blue)
-//            .cornerRadius(16)
-//            .offset(y: offset)
-//    }
-//    
-//    public init(maxHeight: CGFloat, offset: CGFloat, @ViewBuilder content: () -> Content) {
-//        self.minHeight = maxHeight
-//        self.maxHeight = maxHeight
-//        self.offset = offset
-//        self.content = content()
-//    }
-//}
-
-
 #Preview {
     ContentView()
 }
